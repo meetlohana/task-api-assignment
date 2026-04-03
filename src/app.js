83e7183f033a0861df23fabefe296a80b4f117f9
meetlohana <meetlohana1@gmail.com>
2026-04-04 01:16:51 +0530

    root route for deployment

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Task API is running 🚀');
+});
+
 app.use('/tasks', taskRoutes);
 
 app.use((err, req, res, next) => {
